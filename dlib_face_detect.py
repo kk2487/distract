3f240ea017999d118d5a26cd48e3bcdf8bd0594e
Your Name <you@example.com>
2020-10-12 11:35:04 +0800

    add mode in save result

--- dlib_face_detect.py
+++ dlib_face_detect.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import cv2 
 import numpy as np
@@ -11,14 +12,12 @@ from PyQt5.QtGui import *
 import PyQt5.QtGui as QtGui
 from PyQt5.QtCore import *
 
-
-
 class VideoInput():
     box_left = 150
     box_right = 150
     box_up = 50
     box_down = 100
-    def start_capture(self, model, fileUrl):
+    def start_capture(self, model, fileUrl, mode):
         cap = cv2.VideoCapture(fileUrl)
         w = 0
         h = 0
@@ -27,11 +26,19 @@ class VideoInput():
         past_x2 = 500
         past_y2 = 500
         ret, frame = cap.read()
-        width, height = frame.shape[:2]
+        height, width = frame.shape[:2]
         self.predicted_class = 0
         output = ""
         font = cv2.FONT_HERSHEY_SIMPLEX
         detector = dlib.get_frontal_face_detector()
+        if(mode == 2):    
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
+            print(fps)
+            print(width)
+            print(height)
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            video_writer = cv2.VideoWriter('./result.avi', fourcc, fps, (width, height))
+        
         n=0
         while(cap.isOpened()):
             filename = "image_" + str(n) + ".jpg"
@@ -88,20 +95,31 @@ class VideoInput():
 
                 out = frame[y1_body:y2_body,x1_body:x2_body]
                 out = cv2.resize(out, (256, 256) )
+                
                 cv2.imshow("detect image", out) 
+                
                 output, model_out = self.predict_output(out , model)
                 self.predicted_class = np.argmax(model_out)
                 cv2.rectangle(mark_mat, (x1, y1), (x2, y2), (0, 255, 0), 4, cv2.LINE_AA)
                 cv2.rectangle(mark_mat, (x1_body,y1_body),(x2_body,y2_body), (255, 0, 0), 4, cv2.LINE_AA)
                 cv2.rectangle(mark_mat, (0, 0), (500, 100), (255, 255, 255), -1, cv2.LINE_AA)
                 cv2.putText(mark_mat,output,(20,70), font, 1.5,(0,0,255),3,cv2.LINE_AA)
-            #cv2.imwrite(filename, mark_mat)
+
+                if(mode == 1):
+                    cv2.imwrite(filename, out)
+
+            if(mode == 2):
+                cv2.imwrite(filename, mark_mat)
+                video_writer.write(mark_mat)
             
             cv2.imshow("Face Detection", mark_mat)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        if(mode == 2):
+            video_writer.release()
         cap.release()
         cv2.destroyAllWindows()
+
 
     def predict_output(self , image , model):
         image = cv2.resize(image, (utils.IMG_SIZE, utils.IMG_SIZE))
@@ -120,6 +138,13 @@ class Qt(QWidget):
 
 
 if __name__ == '__main__':
+    if(len(sys.argv) < 2):
+        mode =0
+    elif(sys.argv[1] == '--convert'):
+        mode = 1
+    elif(sys.argv[1] == '--result'):
+        mode = 2
+
     network = cnn.CNN()
     model = network.load_model()
     video = VideoInput()
@@ -127,4 +152,4 @@ if __name__ == '__main__':
     process = Qt()
     mv_fileUrl = process.mv_Chooser()
     print(mv_fileUrl)
-    video.start_capture(model, mv_fileUrl)
+    video.start_capture(model, mv_fileUrl, mode)
